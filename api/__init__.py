8538f11b8c67b59a13c479ab40bdfb9a6d001ab1
pawanpdn-671 <pawanpradhanpc671@gmail.com>
2024-01-21 21:00:08 +0530

    update:initialization

---
+++ api/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+from os import path
+
+
+def create_app():
+   app = Flask(__name__)
+   app.config['SECRET_KEY'] = 'hhjkfdsjfddf jfkdsf'
+
+
+   from .views import views
+   app.register_blueprint(views, url_prefix='/api')
+
+   return app
+
